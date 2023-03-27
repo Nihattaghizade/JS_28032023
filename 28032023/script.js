40c829be5833ce940dfe25ac4bded70e66e38f02
Nihat <nihattagizade2003@gmail.com>
2023-03-27 21:31:22 +0400

    js

---
+++ 28032023/script.js
@@ -0,0 +1,11 @@
+let number = prompt('Enter number');
+
+if(number == 0){
+    console.log('Number is zero');
+}
+else if(number > 0){
+    console.log('Number is positive');
+}
+else{
+    console.log('Number is negative');
+}
